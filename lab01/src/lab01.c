e47d8e04f74d4ad5556d5d11cd4c6a1bc29bd0d4
Renzukon <pnguyenhoang@hotmail.com>
2018-09-13 12:27:56 -0400

    Lab 01 :: Finished

--- lab01/src/lab01.c
+++ lab01/src/lab01.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("Hello World!!!"); /* prints Hello World!!! */
+	puts("Hello World from QNX Neutrino RTOS!"); /* prints Hello World from QNX Neutrino RTOS! */
+	puts("@author Peter Nguyen (Nguy0381@algonquinlive.com)");
+	puts("I am the oldest of 3 children in my family");
 	return EXIT_SUCCESS;
 }
